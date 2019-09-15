26feb7145178062997768a61b324eca949c04caa
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-09-15 15:24:58 -0600

    Solve Collatz Sequence and Count Words
    Add solutions to Coders Workshop exercises Collatz Sequence and Count
    Words. Update `build.sbt`.

--- build.sbt
+++ build.sbt
@@ -1,12 +1,12 @@
 lazy val root = (project in file("."))
   .settings(
     organization := "com.martinbrosenberg",
-    name := "ScalaProblems",
+    name := "ScalaExercises",
     version := "0.0.1-SNAPSHOT",
-    scalaVersion := "2.12.8",
+    scalaVersion := "2.12.10",
     libraryDependencies ++= Seq(
       "com.github.dwickern" %% "scala-nameof" % "1.0.3"  % "provided",
       "org.scalacheck"      %% "scalacheck"   % "1.14.0" % "test",
-      "org.scalatest"       %% "scalatest"    % "3.0.5"  % "test",
+      "org.scalatest"       %% "scalatest"    % "3.0.8"  % "test",
     ),
   )
---
+++ src/test/scala/com/martinbrosenberg/exercises/codersworkshop/CollatzSequence_Spec.scala
@@ -0,0 +1,30 @@
+package com.martinbrosenberg.exercises.codersworkshop
+
+import com.martinbrosenberg.exercises.BaseSpec
+import com.martinbrosenberg.exercises.codersworkshop.CollatzSequence._
+import com.martinbrosenberg.exercises.codersworkshop.CollatzSequence_Spec._
+
+class CollatzSequence_Spec extends BaseSpec {
+
+  for (TestCase(start, steps) <- testCases) {
+    s"collatzSequence should take $steps steps when starting from $start" in {
+      assertResult(steps)(collatzSequence(start))
+    }
+  }
+
+}
+
+object CollatzSequence_Spec {
+
+  private case class TestCase(start: Int, steps: Int)
+
+  private val testCases = Seq[TestCase](
+    TestCase(16, 4),
+    TestCase(21, 7),
+    TestCase(24, 10),
+    TestCase(30, 18),
+    TestCase(18, 20),
+    TestCase(25, 23),
+  )
+
+}
