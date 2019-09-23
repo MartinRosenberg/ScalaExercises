5415e5fe0ad4174fdd70ec83bf2b7a82fc6508d7
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-09-23 15:39:18 -0600

    Add Exercism track exercises 6-9

---
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/track/E08_Robot/Bearing.scala
@@ -0,0 +1,17 @@
+package com.martinbrosenberg.exercises.exercism.track.E08_Robot
+
+import Direction._
+
+object Bearing extends Enumeration {
+  type Bearing = Value
+
+  val North, East, South, West = Value
+
+  def apply(bearing: Bearing, dir: Direction): Bearing =
+    Bearing(bearing.id + dir.id match {
+      case -1 => 3
+      case  4 => 0
+      case  n => n
+    })
+
+}
