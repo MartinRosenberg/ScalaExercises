3e6e2615b44e306bf0abda8a547b02ec37d57086
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-05 22:33:23 -0600

    Add 1 more Exercism side exercise (#11)

---
+++ src/test/scala/com/martinbrosenberg/exercises/exercism/side/KindergartenGarden_Spec.scala
@@ -0,0 +1,64 @@
+package com.martinbrosenberg.exercises.exercism.side
+
+import com.martinbrosenberg.exercises.exercism.ExercismBaseSpec
+import com.martinbrosenberg.exercises.exercism.side._KindergartenGarden.{Garden, Plant}
+
+/** @version 1.1.1 */
+class KindergartenGarden_Spec extends ExercismBaseSpec {
+
+  test("partial garden - garden with single student") {
+    Garden.defaultGarden("RC\nGG").plants("Alice") should be(
+      List(Plant.Radishes, Plant.Clover, Plant.Grass, Plant.Grass))
+  }
+
+  test("partial garden - different garden with single student") {
+    Garden.defaultGarden("VC\nRC").plants("Alice") should be(
+      List(Plant.Violets, Plant.Clover, Plant.Radishes, Plant.Clover))
+  }
+
+  test("partial garden - garden with two students") {
+    Garden.defaultGarden("VVCG\nVVRC").plants("Bob") should be(
+      List(Plant.Clover, Plant.Grass, Plant.Radishes, Plant.Clover))
+  }
+
+  test(
+    "multiple students for the same garden with three students, partial garden - second student's garden") {
+    Garden.defaultGarden("VVCCGG\nVVCCGG").plants("Bob") should be(
+      List(Plant.Clover, Plant.Clover, Plant.Clover, Plant.Clover))
+  }
+
+  test(
+    "multiple students for the same garden with three students, partial garden - third student's garden") {
+    Garden.defaultGarden("VVCCGG\nVVCCGG").plants("Charlie") should be(
+      List(Plant.Grass, Plant.Grass, Plant.Grass, Plant.Grass))
+  }
+
+  test("full garden - first student's garden") {
+    Garden
+      .defaultGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+      .plants("Alice") should be(
+      List(Plant.Violets, Plant.Radishes, Plant.Violets, Plant.Radishes))
+  }
+
+  test("full garden - second student's garden") {
+    Garden
+      .defaultGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+      .plants("Bob") should be(
+      List(Plant.Clover, Plant.Grass, Plant.Clover, Plant.Clover))
+  }
+
+  test("full garden - second to last student's garden") {
+    Garden
+      .defaultGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+      .plants("Kincaid") should be(
+      List(Plant.Grass, Plant.Clover, Plant.Clover, Plant.Grass))
+  }
+
+  test("full garden - last student's garden") {
+    Garden
+      .defaultGarden("VRCGVVRVCGGCCGVRGCVCGCGV\nVRCCCGCRRGVCGCRVVCVGCGCV")
+      .plants("Larry") should be(
+      List(Plant.Grass, Plant.Violets, Plant.Clover, Plant.Violets))
+  }
+
+}
