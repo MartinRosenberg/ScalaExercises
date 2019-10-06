af8c07e9cdc44ea71aafd391e471f80436d251b5
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-05 22:33:01 -0600

    Add 1 more Exercism side exercise

---
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/side/_KindergartenGarden/Garden.scala
@@ -0,0 +1,31 @@
+package com.martinbrosenberg.exercises.exercism.side._KindergartenGarden
+
+import Plant._
+import Garden._
+
+case class Garden(garden: Seq[(Plant, Plant)]) {
+
+  def plants(student: String): Seq[Plant] = {
+    val studNum = Students(student)
+    garden
+      .grouped(2)
+      .toSeq
+      .slice(studNum, studNum + 1)
+      .flatMap { case Seq((p1, p2), (p3, p4)) => Seq(p1, p3, p2, p4) }
+  }
+
+}
+
+object Garden {
+
+  private final val Students = Seq(
+    "Alice", "Bob", "Charlie", "David", "Eve", "Fred",
+    "Ginny", "Harriet", "Ileana", "Joseph", "Kincaid", "Larry"
+  ).zipWithIndex.toMap
+
+  def defaultGarden(s: String): Garden = {
+    val Array(row1, row2) = s.split('\n').map(_.map(Plant(_)))
+    Garden(row1.zip(row2))
+  }
+
+}
