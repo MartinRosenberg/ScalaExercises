4e6b9b941d19d71189807888b87ca1f63ebc5681
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-05 22:27:35 -0600

    Add 2 more Exercism side exercises (#10)

---
+++ src/test/scala/com/martinbrosenberg/exercises/exercism/side/Series_Spec.scala
@@ -0,0 +1,28 @@
+package com.martinbrosenberg.exercises.exercism.side
+
+import com.martinbrosenberg.exercises.exercism.ExercismBaseSpec
+
+/** @version created manually */
+class Series_Spec extends ExercismBaseSpec {
+
+  test("slices of one") {
+    Series.slices(1, "") should be (List())
+    Series.slices(1, "01234") should be (List(List(0), List(1), List(2),
+      List(3), List(4)))
+  }
+
+  test("slices of two") {
+    Series.slices(2, "") should be (List())
+    Series.slices(2, "01") should be (List(List(0, 1)))
+    Series.slices(2, "01234") should be (List(List(0, 1), List(1, 2), List(2, 3),
+      List(3, 4)))
+  }
+
+  test("slices of three") {
+    Series.slices(3, "") should be (List())
+    Series.slices(3, "012") should be (List(List(0, 1, 2)))
+    Series.slices(3, "01234") should be (List(List(0, 1, 2), List(1, 2, 3),
+      List(2, 3, 4)))
+  }
+
+}
