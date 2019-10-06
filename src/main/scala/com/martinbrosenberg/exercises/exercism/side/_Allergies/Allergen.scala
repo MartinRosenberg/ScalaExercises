99d446735eda5a8d3ab9cb7e2db10422ba927f23
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2019-10-05 22:27:11 -0600

    Add 2 more Exercism side exercises

---
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/side/_Allergies/Allergen.scala
@@ -0,0 +1,9 @@
+package com.martinbrosenberg.exercises.exercism.side._Allergies
+
+object Allergen extends Enumeration {
+
+  type Allergen = Value
+
+  val Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats = Value
+
+}
