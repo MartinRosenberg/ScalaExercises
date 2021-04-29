d5a214fd1eaa6dd5fa5c478f0a5896a3eeb7848a
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2021-04-29 12:10:23 -0500

    Add error handling to CollatzSequence

--- src/main/scala/com/martinbrosenberg/exercises/codersworkshop/CollatzSequence.scala
+++ src/main/scala/com/martinbrosenberg/exercises/codersworkshop/CollatzSequence.scala
@@ -1,6 +1,7 @@
 package com.martinbrosenberg.exercises.codersworkshop
 
 import scala.annotation.tailrec
+import scala.util.Try
 
 /** A Collatz sequence in mathematics can be defined as follows. Starting with
   * any positive integer `n`:
@@ -15,12 +16,14 @@ import scala.annotation.tailrec
   */
 object CollatzSequence {
 
-  def collatzSequence(n: Int): Int = {
+  def collatzSequence(n: Int): Try[Int] = Try {
+    require(n > 0, "The Collatz conjecture is only valid for positive integers.")
+
     @tailrec
-    def run(last: Int, count: Int): Int = last match {
+    def run(x: Int, count: Int): Int = x match {
       case 1 => count
-      case n if n % 2 == 0 => run(last / 2, count + 1)
-      case _ => run(last * 3 + 1, count + 1)
+      case _ if x % 2 == 0 => run(x / 2, count + 1)
+      case _ => run(x * 3 + 1, count + 1)
     }
     run(n, 0)
   }
