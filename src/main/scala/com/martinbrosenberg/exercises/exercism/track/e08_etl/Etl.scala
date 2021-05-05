c543e09672a094623ee0921e35ac4ed4450b24f4
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2021-05-05 00:46:44 -0500

    Update Exercism exercise Etl

--- src/main/scala/com/martinbrosenberg/exercises/exercism/track/e08_etl/Etl.scala
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/track/e08_etl/Etl.scala
@@ -1,13 +1,12 @@
 package com.martinbrosenberg.exercises.exercism.track.e08_etl
 
 object Etl {
-
   type Letter = String
   type Score = Int
 
-  def transform(byScore: Map[Score, Seq[Letter]]): Map[Letter, Score] = for {
-    (score, letters) <- byScore
-    letter           <- letters
-  } yield letter.toLowerCase -> score
-
+  def transform(lettersByScore: Map[Score, Seq[Letter]]): Map[Letter, Score] =
+    for {
+      (score, letters) <- lettersByScore
+      letter           <- letters
+    } yield letter.toLowerCase -> score
 }
