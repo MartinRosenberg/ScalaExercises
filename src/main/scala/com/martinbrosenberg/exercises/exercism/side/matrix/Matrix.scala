be237bef491637769502b15460211fa452fe9a79
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2021-05-05 14:21:57 -0500

    Update Exercism exercise Matrix

--- src/main/scala/com/martinbrosenberg/exercises/exercism/side/matrix/Matrix.scala
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/side/matrix/Matrix.scala
@@ -1,16 +1,15 @@
 package com.martinbrosenberg.exercises.exercism.side.matrix
 
+import com.martinbrosenberg.exercises.exercism.side.matrix.Matrix.toInts
+
 case class Matrix(s: String) {
-
-  private lazy val rows = s
-    .split("\\R")
-    .map(_.split("\\s").map(_.toInt).toIndexedSeq)
-    .toIndexedSeq
-
-  private lazy val columns = rows.transpose
+  private val rows = s.split("\\R").map(toInts).toVector
+  private val columns = rows.transpose
 
   def row(n: Int): Seq[Int] = rows(n)
-
   def column(n: Int): Seq[Int] = columns(n)
+}
 
+object Matrix {
+  private def toInts(s: String) = s.split("\\s").map(_.toInt).toVector
 }
