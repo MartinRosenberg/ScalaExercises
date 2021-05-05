1ed376cc81cfe51482f4406c417b3306d8a37591
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2021-05-05 00:22:40 -0500

    Update Exercism exercise Hamming

--- src/main/scala/com/martinbrosenberg/exercises/exercism/track/e07_hamming/Hamming.scala
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/track/e07_hamming/Hamming.scala
@@ -1,9 +1,10 @@
 package com.martinbrosenberg.exercises.exercism.track.e07_hamming
 
 object Hamming {
+  type Strand = String
+  type Distance = Int
 
-  def distance(a: String, b: String): Option[Int] =
+  def distance(a: Strand, b: Strand): Option[Distance] =
     if (a.length != b.length) None
     else Some(a.zip(b).count { case (baseA, baseB) => baseA != baseB })
-
 }
