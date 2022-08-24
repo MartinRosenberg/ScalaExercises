d52b5cd2872c35a7fc24ef4baf26cbee5c37acdd
Martin Rosenberg <Martin.B.Rosenberg@gmail.com>
2022-08-23 20:06:45 -0500

    Correct Exercism exercise Anagram

--- src/main/scala/com/martinbrosenberg/exercises/exercism/side/anagram/Anagram.scala
+++ src/main/scala/com/martinbrosenberg/exercises/exercism/side/anagram/Anagram.scala
@@ -2,11 +2,12 @@ package com.martinbrosenberg.exercises.exercism.side.anagram
 
 object Anagram {
 
-  def findAnagrams(word: String, candidates: Seq[String]): Seq[String] = {
-    val sorted = word.sorted
-    candidates.filter(c =>
-      !c.equalsIgnoreCase(word) && c.sorted.equalsIgnoreCase(sorted)
-    )
-  }
+	def findAnagrams(word: String, candidates: Seq[String]): Seq[String] = {
+		val w = word.toLowerCase
+		candidates.filter { candidate =>
+			val c = candidate.toLowerCase
+			c != w && c.sorted == w.sorted
+		}
+	}
 
 }
